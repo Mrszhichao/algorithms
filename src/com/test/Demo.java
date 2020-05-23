de1ec0513c445762f93e1268f5895d6a44ec71c8
Mrszhichao <15771349853@163.com>
2020-05-23 17:29:32 +0800

    修改文件

--- src/com/test/Demo.java
+++ src/com/test/Demo.java
@@ -1,13 +1,30 @@
 package com.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * @description:
+ * @description: 数组转换成list
  * @author: Andy
  * @date: 2020/4/4 18:13
  */
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World123456");
+
+        int[] a = {1, 2, 3, 4};
+        String[] strings = {"1", "2"};
+        List as = Arrays.asList(a);
+        // 会报异常，因为它实现的的类型是java.util.Arrays，它的内部没有实现相关方法
+        // as.add(11);
+        // 用java8的stream操作
+        List<Integer> collect = Arrays.stream(a).boxed().collect(Collectors.toList());
+        // 将不可变类转换成list
+        List<String> list = new ArrayList<String>();
+        Collections.addAll(list, strings);
+
     }
 }
